970ed4c2a4d56f8a3cb438bc8dd75cd9988a9a31
Celso Aleixo celso.aleixo@uello.com.br <celso.aleixo@uello.com.br>
2024-01-24 15:24:50 -0300

    create api users resource and refactories

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,10 +3,20 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserRepository
 {
     public function __construct(protected User $user)
     {
+    }
+
+    public function getPaginate(int $totalPerPage = 15, int $page = 1, string $filter = ''): LengthAwarePaginator
+    {
+        return $this->user->where(function ($query) use ($filter) {
+            if ($filter !== '') {
+                $query->where('name', 'like', "%{$filter}%");
+            }
+        })->paginate($totalPerPage, ['*'], 'page', $page);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/', static fn () => response()->json(['message' => 'ok']));
