8a3e45a065f5570e1f9f3c8b8c3165d8cf8c888a
Celso Aleixo celso.aleixo@uello.com.br <celso.aleixo@uello.com.br>
2024-01-24 16:28:52 -0300

    store new user using DTO object

---
+++ app/DTO/Users/CreateUserDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DTO\Users;
+
+class CreateUserDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password
+    ) {
+        //
+    }
+}
